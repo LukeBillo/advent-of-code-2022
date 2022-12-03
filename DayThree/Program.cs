0ed0a45e1385503192718b673559afde8c5f9d34
LukeBillo <luke.o.billington@gmail.com>
2022-12-03 21:00:13 +0000

    Day three

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,3 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var backpacks = await File.ReadAllLinesAsync("input.txt");
 
-Console.WriteLine("Hello, World!");
+var sumOfPriorities = backpacks
+    .Chunk(3)
+    .Select(GetDuplicateItem)
+    .Select(GetPriority)
+    .Sum();
+
+Console.WriteLine(sumOfPriorities);
+
+List<(string firstCompartment, string secondCompartment)> SplitBackpackCompartments(string[] backpacks)
+{
+    var compartments = new List<(string first, string second)>();
+    
+    foreach (var backpack in backpacks)
+    {
+        var halfway = backpack.Length / 2;
+        var firstCompartment = backpack[..halfway];
+        var secondCompartment = backpack[halfway..];
+        
+        compartments.Add((firstCompartment, secondCompartment));
+    }
+
+    return compartments;
+}
+
+char GetDuplicateItem(string[] groupedBackpacks)
+{
+    var first = groupedBackpacks[0];
+    var second = groupedBackpacks[1];
+    var third = groupedBackpacks[2];
+
+    return first
+        .Intersect(second)
+        .Intersect(third)
+        .Single();
+}
+
+int GetPriority(char item)
+{
+    const int startOfLowercaseAscii = 96;
+    const int endOfLowercaseAscii = 122;
+
+    const int startOfUppercaseAscii = 64;
+    const int endOfUppercaseAscii = 90;
+    
+    var asciiValue = (int)item;
+
+    return asciiValue switch
+    {
+        > startOfLowercaseAscii and <= endOfLowercaseAscii => asciiValue - 96,
+        > startOfUppercaseAscii and <= endOfUppercaseAscii => asciiValue - 38,
+        _ => throw new ArgumentOutOfRangeException()
+    };
+}
